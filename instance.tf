4f8374a2466df30de146e994073b794aabb8ca6e
shubhdev79 <51513309+shubhdev79@users.noreply.github.com>
2019-09-03 04:34:02 -0700

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "web_server" {
 ami = "ami-034e1d78dd9d4a332"
 instance_type = "t2.micro"
 subnet_id = "${aws_subnet.Mgmt-Subnet.id}"
-security_groups = [""TESTEBSSecurityGroup""]
+security_groups = ["TESTEBSSecurityGroup"]
 associate_public_ip_address = "true"
 
 tags = {
